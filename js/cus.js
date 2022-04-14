bccedc0550d91be056a430641eb937c72088a500
Hein-HtetSan <heinhtetsan33455@gmail.com>
2022-04-14 15:29:40 +0630

    0.2.0

--- js/cus.js
+++ js/cus.js
@@ -14,9 +14,39 @@ numRatingBtn.forEach(function(btn) {
             }
         })
         btn.classList.toggle('active')
+        if(btn.classList.contains('active') && current.classList.contains('num1')){
+            number.textContent = 1;
+        } else if (btn.classList.contains('active')  && current.classList.contains('num2')){
+            number.textContent = 2;
+        } else if (btn.classList.contains('active')  && current.classList.contains('num3')){
+            number.textContent = 3;
+        } else{
+            number.textContent = 4;
+        }
     })
 })
 
+var loader;
+function loadNow(opacity) {
+    if (opacity <= 0){
+        displayContent();
+    }
+    else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05)
+        }, 150)
+    }
+}
+function displayContent() {
+    loader.style.display = 'none';
+    document.querySelector('.container').style.display = 'gird';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loader = document.querySelector('.loader');
+    loadNow(1);
+});
 
 
 
